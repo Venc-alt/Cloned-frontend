358743c739ce8519fecb58985cdedea53749c960
Vasny Navas <vasny@interprenet.net>
2024-10-31 16:33:19 -0500

    Fix export for getReservations in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,10 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL, 
 });
 
-
-
 export const registerUser = (userData) => api.post('/users/register', userData);
 export const getGateways = () => api.get('/gateways');
 export const reserveGateway = (reservationData) => api.post('/reservations', reservationData);
+export const getReservations = () => api.get('/reservations'); // Ensure this is present
 
 export default api;
