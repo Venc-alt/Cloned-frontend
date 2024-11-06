3b955cdd7a7584faa8fc37050670a153285e1a93
Vasny Navas <vasny@interprenet.net>
2024-11-06 13:23:12 -0600

    Initial commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,3 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-// });
-
-// export const registerUser = (userData) => api.post('/users/register', userData);
-// export const getGateways = () => api.get('/gateways');
-// export const reserveGateway = (reservationData) => api.post('/reservations', reservationData);
-// export const getReservations = () => api.get('/reservations');
-// export const cancelReservation = (reservationId) => api.delete(`/reservations/${reservationId}`);
-
-// export default api;
-
-
 
 import axios from 'axios';
 
